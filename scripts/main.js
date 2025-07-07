94aac659a39d7d1ee2150006c0096510740562c2
高文硕 <90968450+gaowenshuo@users.noreply.github.com>
2025-07-07 14:25:09 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Wenshuo Gao / 高文硕";
+
+const myDate = document.querySelector("#date");
+myDate.textContent = new Date().getFullYear() + "年" + (new Date().getMonth() + 1) + "月" + new Date().getDate() + "日";
+
